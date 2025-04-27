783dc43c2ff5e78e0de5568d41d2ee29a4aa1da8
JosieVi <o.s.marushkina@gmail.com>
2025-04-28 01:11:06 +0300

    Update HW19 Task 2 based on reviewer comments

--- src/ui/tests/sales-portal/login-hw19-2.spec.ts
+++ src/ui/tests/sales-portal/login-hw19-2.spec.ts
@@ -11,7 +11,7 @@ test.describe('[UI] [Sales Portal] [Login] HW19-2', () => {
         await page.locator('#emailinput').fill(USERNAME);
         await page.locator('#passwordinput').fill(PASSWORD);
         await page.getByRole('button', { name: 'Login' }).click();
-        await page.waitForLoadState('networkidle');
+        await expect(page.locator('.spinner-border')).toHaveCount(0);
         //await page.waitForSelector(`a#dropdownUser1 strong`);
         await expect(page.locator(`a#dropdownUser1 strong`)).toHaveText(USERNAME_AFTER_LOGIN);
         await expect(page.getByRole('heading', { name: 'Welcome to Sales Management Portal' })).toBeVisible();
