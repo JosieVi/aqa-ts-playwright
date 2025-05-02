66ba985e8f629b6d58f734160993a1d34904facc
JosieVi <o.s.marushkina@gmail.com>
2025-05-03 02:44:02 +0300

    Update checkout-hw-20-2.spec.ts (Task 2 HW-20) based on reviewers comments

--- src/ui/tests/demo-shopping-card/checkout-hw-20-2.spec.ts
+++ src/ui/tests/demo-shopping-card/checkout-hw-20-2.spec.ts
@@ -1,59 +1,38 @@
-/*
-- Открыть url https://anatoly-karpovich.github.io/demo-shopping-cart/
-  - Добавить продукты 2,4,6,8,10
-  - Завалидировать бейдж с количеством
-  - Открыть чекаут
-  - Завалидировать сумму и продукты
-  - Ввести все найденные вами промокоды (вспоминаем первую лекцию)
-  - Завалидировать конечную сумму
-  - Зачекаутиться
-  - Завалидировать сумму
-*/
-
 import { test, expect, Page, Locator } from '@playwright/test';
-import { time } from 'console';
+
+interface IPromoCode {
+    [key: string]: string,
+}
 
 const URL = 'https://anatoly-karpovich.github.io/demo-shopping-cart/';
-const totalDiscount = 0.45;
-let PRODUCTS = [2, 4, 6, 8, 10];
+const PRODUCTS: string[] = ['Product 2', 'Product 4', 'Product 6', 'Product 8', 'Product 10'];
 const promocodes = {
     'CODE5': '5-PERCENT-FOR-UTILS',
     'CODE7': 'JAVA-FOR-BOOMERS',
     'CODE8': 'NO-PYTHON',
     'CODE10': 'HOT-COURSE',
-    'CODE10SECOND': '10-PERCENT-FOR-REDEEM',
+    'CODESECOND10': '10-PERCENT-FOR-REDEEM',
     'CODE15': '15-PERCENT-FOR-CSS',
 }
-
-interface ICartProductPrices {
-    [key: string]: number,
-}
+const totalDiscount = (100 - Object.keys(promocodes)
+    .map(key => key.match(/\d+$/))
+    .map(match => parseInt(match![0]))
+    .reduce((acc, num) => acc + num, 0)) / 100;
 
 test.describe('[UI] [Demo Shopping Card] [Checkout] HW20-2', () => {
     test('Should Add Products To Cart', async ({ page }) => {
         await page.goto(URL);
-        for (let productNumber of PRODUCTS) {
-            await addToCardButton(`Product ${productNumber}`, page).click();
-        }
+        await addProductsToCard(PRODUCTS, page);
         await expect(page.locator('.badge')).toHaveText(PRODUCTS.length.toString());
+
+        let cartProductPrices = await Promise.all(PRODUCTS.map((productName) => getProductPrice(productName, page)));
+        const totalPrice = cartProductPrices.reduce((acc, price) => acc + price, 0);
+
         await page.getByRole('button', { name: 'Shopping cart' }).click();
         await expect(page.locator('[data-product-id]')).toHaveCount(PRODUCTS.length);
+        await expect(page.locator('#total-price')).toHaveText(`$${totalPrice.toFixed(2)}`);
 
-        let cartProductPrices: ICartProductPrices = {};
-        for (let productNumber of PRODUCTS) {
-            await expect(getCartProductLocator(`Product ${productNumber}`, page)).toBeVisible();
-            let currentProductPrice = await getProductPrice(`Product ${productNumber}`, page);
-            cartProductPrices = { ...cartProductPrices, [`Product ${productNumber}`]: currentProductPrice };
-        }
-        const totalPrice = Object.values(cartProductPrices).reduce((acc, price) => acc + price, 0);
-        await expect(page.locator('#total-price')).toHaveText(`$${totalPrice}.00`);
-
-        await inputPromoCode(page, promocodes.CODE5);
-        await inputPromoCode(page, promocodes.CODE7);
-        await inputPromoCode(page, promocodes.CODE8);
-        await inputPromoCode(page, promocodes.CODE10);
-        await inputPromoCode(page, promocodes.CODE10SECOND);
-        await inputPromoCode(page, promocodes.CODE15);
+        await inputPromoCode(promocodes, page);
         await expect(page.locator('#total-price')).toHaveText(`$${(totalPrice * totalDiscount).toFixed(2)} (-$${(totalPrice * (1 - totalDiscount)).toFixed(1)})`);
         await page.getByRole('button', { name: 'Continue to checkout' }).click();
         await expect(page.getByText('Thanks for ordering!')).toBeVisible();
@@ -61,25 +40,47 @@ test.describe('[UI] [Demo Shopping Card] [Checkout] HW20-2', () => {
     });
 });
 
-function addToCardButton(product: string, page: Page): Locator {
-    return page.locator('div.card-body').filter({ has: page.getByText(`${product}`) }).getByRole('button', { name: 'Add to card' });
+
+function findProductLocators(productName: string[], page: Page): Locator[] {
+    let productForOrderLocators: Locator[] = [];
+    for (let i = 0; i < productName.length; i++) {
+        productForOrderLocators[i] = page
+            .locator('div.card-body')
+            .filter({ has: page.getByText(`${productName[i]}`) })
+            .getByRole('button', { name: 'Add to card' });
+    }
+    return productForOrderLocators;
 }
 
-function getCartProductLocator(product: string, page: Page): Locator {
-    return page.locator('[data-product-id]').getByRole('heading', { name: `${product}` });
+async function addProductsToCard(productName: string[], page: Page): Promise<void> {
+    const productForOrderLocators = findProductLocators(productName, page);
+    for (let i = 0; i < productForOrderLocators.length; i++) {
+        await productForOrderLocators[i].click();
+    }
 }
 
-async function getProductPrice(product: string, page: Page): Promise<number> {
-    const productLocator = await page.locator(`//li[.//h5[text()='${product}']]//span[@class=\'text-muted fw-bold\']`);
-    const priseAsText = await productLocator.innerText();
+function getProductPriceSpan(productName: string, page: Page): Locator {
+    return page
+        .locator("div.card-body")
+        .filter({
+            has: page.getByText(`${productName}`),
+        })
+        .locator("span");
+}
+
+async function getProductPrice(productName: string, page: Page): Promise<number> {
+    // const productLocator = await page.locator(`//li[.//h5[text()='${product}']]//span[@class=\'text-muted fw-bold\']`);
+    // const priseAsText = await productLocator.innerText();
+    const productPriceSpan = getProductPriceSpan(productName, page);
+    const priseAsText = await productPriceSpan.innerText();
     const price = priseAsText.replace('$', '')
     return +price;
 }
 
-async function inputPromoCode(page: Page, promocode: string): Promise<void> {
-    await page.getByPlaceholder('Promo code').fill(promocode);
-    await page.getByRole('button', { name: 'Redeem' }).click();
-    await expect(page.locator('#rebates-container li').getByText(promocode)).toBeVisible();
-
+async function inputPromoCode(promocode: IPromoCode, page: Page,): Promise<void> {
+    for (const code of Object.values(promocode)) {
+        await page.getByPlaceholder('Promo code').fill(code);
+        await page.getByRole('button', { name: 'Redeem' }).click();
+        await expect(page.locator('#rebates-container li').getByText(code)).toBeVisible();
+    }
 }
-
